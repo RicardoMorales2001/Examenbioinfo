6f53283b31ae8a889fca61b017cc52ea93640124
RicardoMorales2001 <95895941+RicardoMorales2001@users.noreply.github.com>
2022-01-14 12:06:31 -0600

    Se copio un archivo de un directorio a otro

--- ejercicio_1101.sh
+++ ejercicio_1101.sh
@@ -1,3 +1,10 @@
 #Ejercicio 10.0.1
 # 1. Dirigirse al directorio CSB/unix/sandbox
 cd Documents/CSB/CSB/unix/sandbox
+# 2. ¿Cuál es el tamaño del archivo Marra2014_data.fasta?
+ls -lh ../data/Marra2014_data.fasta
+# Respuesta: 553K
+# Se utiliza el comando ls -l debido a que este enlista y detalla la información del archivo en cuestio y se agrega la -h para que esta información pueda ser leida.
+# 3. Cree una copia de Marra2014_data.fasta en sandbox y asígnele el nombre my_file.fasta.
+cp ../data/Marra2014_data.fasta my_file.fasta
+# Se usa el comando cp para copiar un archivo de un directorio a otro.
